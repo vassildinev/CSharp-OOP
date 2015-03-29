0f455646ce009a11d37f31387554c862d0e4a7ad
Васил Динев <vassildinev@gmail.com>
2015-03-29 20:45:26 +0300

    Added more features.

---
+++ JustBlueberryTeamProject/JustBlueberry/BlueberryMain.cs
@@ -0,0 +1,30 @@
+﻿namespace JustBlueberry
+{
+    using System.Collections.Generic;
+    using JustBlueberry.Interfaces;
+
+    public class BlueberryMain
+    {
+        // Define constant dimensions for the game world.
+        const int DEFAULT_WORLD_ROWS = 30;
+        const int DEFAULT_WORLD_COLS = 80;
+
+        public static void Main()
+        {
+            var renderer = new ConsoleRenderer(DEFAULT_WORLD_ROWS, DEFAULT_WORLD_COLS);
+
+            var hadronOperator = new Operator();
+
+            var physicalWorldObjects = new List<IMatter>()
+            {
+                BlueberryFactory.CreateDarkBlueberry(new Vector(2,2)),
+                BlueberryFactory.CreateNervousBlueberry(new Vector(2, 40))
+            };
+
+
+            var engine = new Engine(renderer, hadronOperator, physicalWorldObjects, 100);
+
+            engine.Run();
+        }
+    }
+}
---
+++ JustBlueberryTeamProject/JustBlueberry/Vector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace JustBlueberry
+{
+    public class Vector
+    {
+        public int DeltaR { get; set; }
+        public int DeltaC { get; set; }
+
+        public Vector(int deltaR, int deltaC)
+        {
+            this.DeltaR = deltaR;
+            this.DeltaC = deltaC;
+        }
+
+    }
+}
